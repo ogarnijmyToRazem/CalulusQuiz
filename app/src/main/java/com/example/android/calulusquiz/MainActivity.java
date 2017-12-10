dc1100a4f54d23a2ae720f1933b173da508e3205
Michal Smigiel <michal.smigiel@onet.pl>
2017-12-10 23:40:07 +0100

    Add formatting

--- app/src/main/java/com/example/android/calulusquiz/MainActivity.java
+++ app/src/main/java/com/example/android/calulusquiz/MainActivity.java
@@ -6,15 +6,15 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     int pointsForRightAnswer = 10;
     int points = 0;
 
-    float answer2Right=102.02f;
-    int answer5Right=2;
+    String answer2Right = "102.02";
+    String answer5Right = "2";
 
     int answer1 = 0;
     String answer2 = "";
@@ -49,6 +49,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submit(View v) {
+        getAnswers();
+        checkAnswers();
+        setPointText();
+        points=0;
     }
 
     public void getAnswers() {
@@ -95,41 +102,47 @@ public class MainActivity extends AppCompatActivity {
         if (answer1 == R.id.answer13) {
             points += pointsForRightAnswer;
         }
-        if(Float.parseFloat(answer2)==answer2Right){
-            points+=pointsForRightAnswer;
+        if (answer2.equals(answer2Right)) {
+            points += pointsForRightAnswer;
         }
-        if(answer31&&!answer32&&answer33&&!answer34){
-            points+=pointsForRightAnswer;
+        if (answer31 && !answer32 && answer33 && !answer34) {
+            points += pointsForRightAnswer;
         }
-        if(!answer41&&answer42&&!answer43&&answer44){
-            points+=pointsForRightAnswer;
+        if (!answer41 && answer42 && !answer43 && answer44) {
+            points += pointsForRightAnswer;
         }
-        if(Integer.parseInt(answer5)==answer5Right){
-            points+=pointsForRightAnswer;
+        if (answer5.equals(answer5Right)) {
+            points += pointsForRightAnswer;
         }
-        if(!answer61&&!answer62&&answer63&&answer64){
-            points+=pointsForRightAnswer;
+        if (!answer61 && !answer62 && answer63 && answer64) {
+            points += pointsForRightAnswer;
         }
-        if(!answer71&&answer72&&answer73&&!answer74){
-            points+=pointsForRightAnswer;
+        if (!answer71 && answer72 && answer73 && !answer74) {
+            points += pointsForRightAnswer;
         }
-        if(answer8==R.id.answer84){
-            points+=pointsForRightAnswer;
+        if (answer8 == R.id.answer84) {
+            points += pointsForRightAnswer;
         }
-        if(answer91&&!answer92&&answer93&&answer94){
-            points+=pointsForRightAnswer;
+        if (answer91 && !answer92 && answer93 && answer94) {
+            points += pointsForRightAnswer;
         }
-        if(!answer101&&!answer102&&answer103&&answer104){
-            points+=pointsForRightAnswer;
+        if (!answer101 && !answer102 && answer103 && answer104) {
+            points += pointsForRightAnswer;
         }
     }
 
+    public void setPointText() {
+        TextView pointTextView = findViewById(R.id.points);
+        String pointText = getString(R.string.point_text, points);
+        pointTextView.setText(pointText);
+        pointTextView.setVisibility(View.VISIBLE);
+    }
 
     public int getAnswerRadioButton(int id1, int id2, int id3, int id4) {
-        RadioButton option1 = (RadioButton) findViewById(id1);
-        RadioButton option2 = (RadioButton) findViewById(id2);
-        RadioButton option3 = (RadioButton) findViewById(id3);
-        RadioButton option4 = (RadioButton) findViewById(id4);
+        RadioButton option1 = findViewById(id1);
+        RadioButton option2 = findViewById(id2);
+        RadioButton option3 = findViewById(id3);
+        RadioButton option4 = findViewById(id4);
         if (option1.isChecked()) {
             return option1.getId();
         } else if (option2.isChecked()) {
@@ -142,12 +155,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String getAnswerEditText(int id) {
-        EditText question = (EditText) findViewById(id);
+        EditText question = findViewById(id);
         return question.getText().toString();
     }
 
     public boolean getAnswerCheckBox(int id) {
-        CheckBox option = (CheckBox) findViewById(id);
+        CheckBox option = findViewById(id);
         return option.isChecked();
     }
 }
